6968a845834188cb556fbd2f236695a405abd519
talitha <mlithhs@gmail.com>
2023-11-11 20:36:32 -0300

    refactor(util): organize and export functions

--- src/repositories/Util.js
+++ src/repositories/Util.js
@@ -1,23 +1,29 @@
-export const findInArrayById = (array, id) => {
-  let result = [];
-  for (const element in array) {
-    if (array[element]["id"] == id) {
-      console.log(array[element]);
-      result = array[element];
-      break;
+const util = {
+
+  findInArrayById: (array, id) => {
+    let result = [];
+    for (const element in array) {
+      if (array[element]["id"] == id) {
+        console.log(array[element]);
+        result = array[element];
+        break;
+      }
     }
+    console.log(result);
+    return result;
+  },
+
+  findInArrayByName: (array, name) => {
+    console.log(array);
+    let result = [];
+    array.forEach((element) => {
+      console.log(element["name"]);
+      if (element["name"] == name) result = element;
+    });
+    console.log(result);
+    return result;
   }
-  console.log(result);
-  return result;
+
 };
 
-export const findInArrayByName = (array, name) => {
-  console.log(array);
-  let result = [];
-  array.forEach((element) => {
-    console.log(element["name"]);
-    if (element["name"] == name) result = element;
-  });
-  console.log(result);
-  return result;
-};
+module.exports = util;
