3aa2cc031fb7f697fa63643410e7d9b01b223b31
talitha <mlithhs@gmail.com>
2023-11-12 21:43:01 -0300

    chore(util): remove console.log statements

--- src/repositories/Util.js
+++ src/repositories/Util.js
@@ -4,23 +4,18 @@ const util = {
     let result = [];
     for (const element in array) {
       if (array[element]["id"] == id) {
-        console.log(array[element]);
         result = array[element];
         break;
       }
     }
-    console.log(result);
     return result;
   },
 
   findInArrayByName: (array, name) => {
-    console.log(array);
     let result = [];
     array.forEach((element) => {
-      console.log(element["name"]);
       if (element["name"] == name) result = element;
     });
-    console.log(result);
     return result;
   }
 
